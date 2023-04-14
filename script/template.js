cf83331c9ed377cc9ea9f46fd81a965af0e60733
ninjaneural <127111336+ninjaneural@users.noreply.github.com>
2023-04-14 18:43:03 +0900

    수정

--- script/template.js
+++ script/template.js
@@ -4,31 +4,209 @@ const TARGET_DIR = 'stable';
 const BASE_SRC = `../${TARGET_DIR}/template.ipynb`;
 
 const checkpoints = [
-    { ipynb: 'abyss_orange_mix_2_webui_colab', url: 'https://huggingface.co/WarriorMama777/OrangeMixs/resolve/main/Models/AbyssOrangeMix2/Pruned/AbyssOrangeMix2_hard_pruned_fp16_with_VAE.safetensors', name: 'AbyssOrangeMix2_hard_pruned_fp16_with_VAE.safetensors' },
-    { ipynb: 'abyssbasil_0_5_webui_colab', url: 'https://civitai.com/api/download/models/11182?type=Model&format=SafeTensor', name: 'AbyssBasil2.safetensors' },
-    { ipynb: 'aniflatmix_webui_colab', url: 'https://civitai.com/api/download/models/29155?type=Model&format=SafeTensor', name: 'aniflatmixAnimeFlatColorStyle_v10.safetensors' },
-    { ipynb: 'blueberrymix_webui_colab', url: 'https://huggingface.co/luizC/blueberry/resolve/main/blueberrymix_10.safetensors', name: 'blueberrymix_10.safetensors' },
-    { ipynb: 'camellia_mix25d_webui_colab', url: 'https://huggingface.co/Powidl43/CamelliaMix/resolve/main/CamelliaMix_2.5D.safetensors', name: 'CamelliaMix_2.5D.safetensors' },
-    { ipynb: 'chillout_mix_webui_colab', url: 'https://huggingface.co/swl-models/chilloutmix/resolve/main/Chilloutmix-non-ema-fp16.safetensors', name: 'Chilloutmix-non-ema-fp16.safetensors' },
-    { ipynb: 'chillout_ni_mix_webui_colab', url: 'https://huggingface.co/swl-models/chilloutmix-ni/resolve/main/chilloutmix-Ni-non-ema-fp16.safetensors', name: 'chilloutmix-Ni-non-ema-fp16.safetensors' },
-    { ipynb: 'chikmix_mix_webui_colab', url: 'https://huggingface.co/KSD2023/chikmix_V2/resolve/main/chikmix_V2.safetensors', name: 'chikmix_V2.safetensors' },
-    { ipynb: 'counterfeit_webui_colab', url: 'https://huggingface.co/ckpt/Counterfeit-V2.0/resolve/main/Counterfeit-V2.0fp16.safetensors', name: 'Counterfeit-V2.0fp16.safetensors' },
-    { ipynb: 'deliberate_webui_colab', url: 'https://huggingface.co/XpucT/Deliberate/resolve/main/Deliberate_v2.safetensors', name: 'Deliberate_v2.safetensors' },
-    { ipynb: 'dreamshaper_webui_colab', url: 'https://huggingface.co/Lykon/DreamShaper/resolve/main/DreamShaper_4BakedVae_fp16.safetensors', name: 'DreamShaper_4BakedVae_fp16.safetensors' },
-    { ipynb: 'expmix_line_webui_colab', url: 'https://huggingface.co/Dorshu/expmixLine_v20/resolve/main/expmixLine_v20.safetensors', name: 'expmixLine_v20.safetensors' },
+    {
+        name: 'AbyssOrangeMix2',
+        type: '2.5D',
+        model: 'https://huggingface.co/WarriorMama777/OrangeMixs',
+        ipynb: 'abyss_orange_mix_2_webui_colab',
+        checkpoint: 'https://huggingface.co/WarriorMama777/OrangeMixs/resolve/main/Models/AbyssOrangeMix2/Pruned/AbyssOrangeMix2_hard_pruned_fp16_with_VAE.safetensors',
+        checkpoint_file: 'AbyssOrangeMix2_hard_pruned_fp16_with_VAE.safetensors',
+        bakedVAE: true,
+    },
+    { name: 'AbyssBasil2', type: '2.5D', model: '', ipynb: 'abyssbasil_0_5_webui_colab', checkpoint: 'https://civitai.com/api/download/models/11182?type=Model&format=SafeTensor', checkpoint_file: 'AbyssBasil2.safetensors' },
+    {
+        name: 'Animeflatmix',
+        type: '2D',
+        model: 'https://civitai.com/models/24387/aniflatmix-anime-flat-color-style-mix',
+        ipynb: 'aniflatmix_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/29155?type=Model&format=SafeTensor',
+        checkpoint_file: 'aniflatmixAnimeFlatColorStyle_v10.safetensors',
+    },
+    {
+        name: 'blueberrymix',
+        type: 'Realistic',
+        model: 'https://civitai.com/models/14323/blueberrymix',
+        ipynb: 'blueberrymix_webui_colab',
+        checkpoint: 'https://huggingface.co/luizC/blueberry/resolve/main/blueberrymix_10.safetensors',
+        checkpoint_file: 'blueberrymix_10.safetensors',
+    },
+    {
+        name: 'CamelliaMix',
+        type: '2.5D',
+        model: 'https://huggingface.co/Powidl43/CamelliaMix',
+        ipynb: 'camellia_mix25d_webui_colab',
+        checkpoint: 'https://huggingface.co/Powidl43/CamelliaMix/resolve/main/CamelliaMix_2.5D.safetensors',
+        checkpoint_file: 'CamelliaMix_2.5D.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'ChilloutMix',
+        type: 'Realistic',
+        model: 'https://huggingface.co/swl-models/chilloutmix',
+        ipynb: 'chillout_mix_webui_colab',
+        checkpoint: 'https://huggingface.co/swl-models/chilloutmix/resolve/main/Chilloutmix-non-ema-fp16.safetensors',
+        checkpoint_file: 'Chilloutmix-non-ema-fp16.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'ChilloutMix-Ni',
+        type: 'Realistic',
+        model: 'https://huggingface.co/swl-models/chilloutmix-ni',
+        ipynb: 'chillout_ni_mix_webui_colab',
+        checkpoint: 'https://huggingface.co/swl-models/chilloutmix-ni/resolve/main/chilloutmix-Ni-non-ema-fp16.safetensors',
+        checkpoint_file: 'chilloutmix-Ni-non-ema-fp16.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'Chikmix',
+        type: '2.5D',
+        model: 'https://civitai.com/models/9871/chikmix',
+        ipynb: 'chikmix_mix_webui_colab',
+        checkpoint: 'https://huggingface.co/KSD2023/chikmix_V2/resolve/main/chikmix_V2.safetensors',
+        checkpoint_file: 'chikmix_V2.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'Counterfeit-V2.0',
+        type: '2D',
+        model: 'https://huggingface.co/gsdf/Counterfeit-V2.0',
+        ipynb: 'counterfeit_webui_colab',
+        checkpoint: 'https://huggingface.co/ckpt/Counterfeit-V2.0/resolve/main/Counterfeit-V2.0fp16.safetensors',
+        checkpoint_file: 'Counterfeit-V2.0fp16.safetensors',
+    },
+    {
+        name: 'Deliberate',
+        type: 'Realistic',
+        model: 'https://huggingface.co/XpucT/Deliberate',
+        ipynb: 'deliberate_webui_colab',
+        checkpoint: 'https://huggingface.co/XpucT/Deliberate/resolve/main/Deliberate_v2.safetensors',
+        checkpoint_file: 'Deliberate_v2.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'DreamShaper',
+        ipynb: 'dreamshaper_webui_colab',
+        model: 'https://huggingface.co/Lykon/DreamShaper',
+        checkpoint: 'https://huggingface.co/Lykon/DreamShaper/resolve/main/DreamShaper_4BakedVae_fp16.safetensors',
+        checkpoint_file: 'DreamShaper_4BakedVae_fp16.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'expmixLine_v2',
+        type: '2D',
+        model: 'https://huggingface.co/AIARTCHAN/expmixLine_v2',
+        ipynb: 'expmix_line_webui_colab',
+        checkpoint: 'https://huggingface.co/Dorshu/expmixLine_v20/resolve/main/expmixLine_v20.safetensors',
+        checkpoint_file: 'expmixLine_v20.safetensors',
+    },
+    {
+        name: 'henmixreal',
+        type: 'Realistic',
+        model: 'https://civitai.com/models/20282/henmixreal',
+        ipynb: 'henmix_v1_webui_colab',
+        checkpoint: 'https://huggingface.co/naonovn/henmix/resolve/main/henmixReal_v10.safetensors',
+        checkpoint_file: 'henmixReal_v10.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'LOFI',
+        type: 'Realistic',
+        model: 'https://civitai.com/models/9052/lofi',
+        ipynb: 'lofi_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/28882?type=Model&format=SafeTensor',
+        checkpoint_file: 'lofi_V2.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'MeinaMix',
+        type: '2D',
+        model: 'https://huggingface.co/Meina/MeinaMix',
+        ipynb: 'meina_mix_webui_colab',
+        checkpoint: 'https://huggingface.co/Meina/MeinaMix/resolve/main/Meina%20V8%20baked%20VAE.safetensors',
+        checkpoint_file: 'MeinaV8bakedVAE.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'MeinaPastel',
+        type: '2D',
+        model: 'https://huggingface.co/Meina/MeinaPastel',
+        ipynb: 'meina_pastel_webui_colab',
+        checkpoint: 'https://huggingface.co/Meina/MeinaPastel/resolve/main/MeinaPastelV3%20-%20Without%20VAE.safetensors',
+        checkpoint_file: 'MeinaPastelV3WithoutVAE.safetensors',
+    },
+    {
+        name: 'mistoonanime',
+        type: '2D',
+        model: 'https://civitai.com/models/24149/mistoonanime',
+        ipynb: 'mistoon_anime_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/28861?type=Model&format=SafeTensor',
+        checkpoint_file: 'mistoonAnime.safetensors',
+    },
+    {
+        name: 'mix-pro-v4',
+        type: '2D',
+        model: 'https://civitai.com/models/7241/mix-pro-v4',
+        ipynb: 'mixpro_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/34559?type=Model&format=SafeTensor',
+        checkpoint_file: 'mixProV4.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'old-fish',
+        type: '2D',
+        model: 'https://civitai.com/models/14978/old-fish',
+        ipynb: 'oldfish_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/22052?type=Model&format=SafeTensor',
+        checkpoint_file: 'OldFish_v11.safetensors',
+    },
+    {
+        name: 'PerfectWorld_V1',
+        type: '2.5D',
+        model: 'https://huggingface.co/naonovn/PerfectWorldAom2hbasilmix',
+        ipynb: 'perfectworld_webui_colab',
+        checkpoint: 'https://huggingface.co/naonovn/PerfectWorldAom2hbasilmix/resolve/main/perfectWorld_perfectWorldBakedVAE.safetensors',
+        checkpoint_file: 'perfectWorld_perfectWorldBakedVAE.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'RealDosmix',
+        type: '2.5D',
+        model: 'https://civitai.com/models/6925/realdosmix',
+        ipynb: 'realdosmix_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/8137?type=Model&format=SafeTensor',
+        checkpoint_file: 'realdosmix.safetensors',
+    },
+    {
+        name: 'realistic-vision',
+        type: 'Realistic',
+        model: 'https://civitai.com/models/4201/realistic-vision-v13-fantasyai',
+        ipynb: 'realistic_vision_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/6987?type=Model&format=SafeTensor',
+        checkpoint_file: 'realisticVisionV13_v13.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'v08_v80',
+        type: '2.5D',
+        ipynb: 'v80_v80_webui_colab',
+        model: 'https://civitai.com/models/18427/v08',
+        checkpoint: 'https://civitai.com/api/download/models/24196?type=Model&format=SafeTensor',
+        checkpoint_file: 'V80_V80.safetensors',
+        bakedVAE: true,
+    },
+    { name: 'yorrrlmix', type: '2.5D', model: 'https://civitai.com/models/17938/yorrrlmix', ipynb: 'yorrrlmix_webui_colab', checkpoint: 'https://civitai.com/api/download/models/30528', checkpoint_file: 'yorrrlmix_v21.safetensors' },
 ];
 
 async function copy_files() {
-
     let templateCode = fs.readFileSync(BASE_SRC, { encoding: 'utf8' });
 
     checkpoints.forEach((item) => {
         console.log(`${item.ipynb} 복사`);
         let code = templateCode;
-        code = code.replaceAll('#template_checkpoint_default#', item.url);
-        code = code.replaceAll('#template_checkpoint_default_name#', item.name);
+        code = code.replaceAll('#template_checkpoint_default#', item.checkpoint);
+        code = code.replaceAll('#template_checkpoint_default_name#', item.checkpoint_file);
         fs.writeFileSync(`../${TARGET_DIR}/${item.ipynb}.ipynb`, code);
-    })
+    });
 
     return true;
 }
