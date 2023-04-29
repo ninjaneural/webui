d458130a1ed1cb1eab679f638b4029e21b256b0c
ninjaneural <127111336+ninjaneural@users.noreply.github.com>
2023-04-29 15:48:01 +0900

    수정

--- script/rename.js
+++ script/rename.js
@@ -4,11 +4,16 @@ const fs = require("fs");
 
 var options = {};
 var basepath = `${process.argv[2]?.replace(/\\/g, '/')}/*.png`;
+var step = process.argv[4] ?? 1;
+var startIndex = process.argv[3] ?? 0;
+if (step) step = parseInt(step);
+if (startIndex === 0 || !!startIndex) startIndex = parseInt(startIndex);
 
 (function () {
     glob(`${basepath}`, options, function (er, files) {
-        let index = process.argv[3] ?? 0;
-        for (element of files) {
+        let index = startIndex + (files.length - 1) * step;
+        let reversed = files.reverse();
+        for (element of reversed) {
             let name = path.basename(element);
             let dir = path.dirname(element);
             newname = ('' + index).padStart(7, '0') + '.png';
@@ -16,7 +21,7 @@ var basepath = `${process.argv[2]?.replace(/\\/g, '/')}/*.png`;
                 [dir, "/", name].join(""),
                 [dir, "/", newname].join("")
             );
-            index++;
+            index -= step;
         }
     });
 })();
