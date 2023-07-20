3077558f2f6a46a40aad5251cdfdc2cdd0f1cd87
ninjaneural <127111336+ninjaneural@users.noreply.github.com>
2023-07-20 13:35:28 +0900

    테스트

--- misc/update_stable.sh
+++ misc/update_stable.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/continue-revolution/sd-webui-animatediff /content/sdd-webui/extensions/sd-webui-animatediff
+git clone https://github.com/continue-revolution/sd-webui-animatediff /content/sdd-webui/extensions/animatediff
