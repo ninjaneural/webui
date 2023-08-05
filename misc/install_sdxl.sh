c9430f4ebe95e32b99af3ce159d94ac517c89da7
ninjaneural <127111336+ninjaneural@users.noreply.github.com>
2023-08-06 03:57:49 +0900

    sdxl 수정

---
+++ misc/install_sdxl.sh
@@ -0,0 +1,2 @@
+git clone -b v1.5.1 https://github.com/AUTOMATIC1111/stable-diffusion-webui /content/ui
+cd /content/ui
---
+++ script/template_sdxl.js
@@ -0,0 +1,141 @@
+const fs = require('fs');
+
+const TARGET_DIR = process.argv[2] ?? 'sdxl';
+const BASE_SRC = `../${TARGET_DIR}/template.ipynb`;
+
+const checkpoints = [
+    {
+        name: 'DreamShaper XL',
+        type: '범용',
+        model: 'https://civitai.com/models/112902/dreamshaper-xl10',
+        ipynb: 'dreamshaper_xl_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/121931?type=Model&format=SafeTensor&size=full&fp=fp16',
+        checkpoint_file: 'DreamShaper_XL_09Alpha.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'AnimeArtDiffusion XL',
+        type: '2D',
+        model: 'https://huggingface.co/AIARTCHAN/expmixLine_v2',
+        ipynb: 'anime_art_diffusion_xl_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/127055?type=Model&format=SafeTensor&size=full&fp=fp16',
+        checkpoint_file: 'AnimeArtDiffusion_XL_Alpha2.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'CounterfeitXL',
+        type: '2D',
+        model: 'https://civitai.com/models/118406/counterfeitxl',
+        ipynb: 'counterfeit_xl_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/128439?type=Model&format=SafeTensor&size=pruned&fp=fp16',
+        checkpoint_file: 'counterfeit_xl.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'YamerMIX SDXL',
+        type: '2.5D',
+        model: 'https://civitai.com/models/84040/sdxl-unstable-diffusers-yamermix',
+        ipynb: 'yamer_mix_xl_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/133118?type=Model&format=SafeTensor&size=full&fp=fp16',
+        checkpoint_file: 'yamer_mix_xl.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'SDVN6-RealXL',
+        type: '실사',
+        model: 'https://civitai.com/models/118114/sdvn6-realxl',
+        ipynb: 'sdvn6_realxl_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/130249?type=Model&format=SafeTensor&size=full&fp=fp16',
+        checkpoint_file: 'sdvn6_realxl.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'DucHaiten-AIart-SDXL',
+        type: '범용',
+        model: 'https://civitai.com/models/118756/duchaiten-aiart-sdxl',
+        ipynb: 'duchaiten_aiart_sdxl_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/133646?type=Model&format=SafeTensor&size=pruned&fp=fp32',
+        checkpoint_file: 'duchaiten_aiart_sdxl.safetensors',
+        bakedVAE: true,
+    },
+    {
+        name: 'ShikiAnimeXL',
+        type: '2D',
+        model: 'https://civitai.com/models/119163/shikianimexl',
+        ipynb: 'shikianimexl_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/129369?type=Model&format=SafeTensor&size=full&fp=fp16',
+        checkpoint_file: 'shikianimexl.safetensors',
+        bakedVAE: false,
+    },
+
+    {
+        name: 'DynaVision XL',
+        type: '3D',
+        model: 'https://civitai.com/models/122606/dynavision-xl-all-in-one-stylized-3d-sfw-and-nsfw-output-no-refiner-needed',
+        ipynb: 'dynavision_xl_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/133559?type=Model&format=SafeTensor&size=pruned&fp=fp16',
+        checkpoint_file: 'dynavision_xl.safetensors',
+        bakedVAE: false,
+    },
+    {
+        name: 'Copax Realistic XL',
+        type: '복합',
+        model: 'https://civitai.com/models/118111/copax-realistic-xl-sdxl10',
+        ipynb: 'copax_realistic_xl_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/133549?type=Model&format=SafeTensor&size=pruned&fp=fp16',
+        checkpoint_file: 'copax_realistic_xl.safetensors',
+        bakedVAE: false,
+    },
+    {
+        name: 'RunDiffusion XL',
+        type: '실사',
+        model: 'https://civitai.com/models/120964/rundiffusion-xl',
+        ipynb: 'rundiffusion_xl_webui_colab',
+        checkpoint: 'https://civitai.com/api/download/models/131579?type=Model&format=SafeTensor&size=full&fp=fp16',
+        checkpoint_file: 'rundiffusion_xl.safetensors',
+        bakedVAE: true,
+    },
+];
+
+async function copy_files() {
+    let templateCode = fs.readFileSync(BASE_SRC, { encoding: 'utf8' });
+    let readme = [];
+    readme.push(`| Colab                                                                                                                                                                                            | Model                                                                                  | VAE  | Memo                    |`);
+    readme.push(`| ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ | -------------------------------------------------------------------------------------- | ---- | ----------------------- |`);
+    checkpoints.forEach((item) => {
+        console.log(`${item.ipynb} 복사`);
+        let code = templateCode;
+        code = code.replaceAll('#template_checkpoint_default#', item.checkpoint);
+        code = code.replaceAll('#template_checkpoint_default_name#', item.checkpoint_file);
+        fs.writeFileSync(`../${TARGET_DIR}/${item.ipynb}.ipynb`, code);
+
+        readme.push(`| [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/ninjaneural/webui/blob/master/${TARGET_DIR}/${item.ipynb}.ipynb) | [${item.name}](${item.model})                    | ${item.bakedVAE ? '' : '선택'} | ${item.type}                      |`)
+    });
+
+    readmeText = readme.join('\n');
+    fs.writeFileSync(`../${TARGET_DIR}/README.md`, readmeText);
+
+    return true;
+}
+
+async function make_readme() {
+    let readme = [];
+    readme.push(`| WebUI 3월버전                                                                                                                                                                                    | WebUI 심플                                                                                                                                                                               | WebUI 최신                                                                                                                                                                                        | Deforum전용                                                                                                                                                                                      | Model                                                                                  | VAE  | Memo                    |`);
+    readme.push(`| ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ | -------------------------------------------------------------------------------------- | ---- | ----------------------- |`);
+    checkpoints.forEach((item) => {
+        readme.push(`| [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/ninjaneural/webui/blob/master/stable/${item.ipynb}.ipynb) | [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/ninjaneural/webui/blob/master/simple/${item.ipynb}.ipynb) | [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/ninjaneural/webui/blob/master/nightly/${item.ipynb}.ipynb) | [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/ninjaneural/webui/blob/master/deforum/${item.ipynb}.ipynb) | [${item.name}](${item.model})                    | ${item.bakedVAE ? '' : '선택'} | ${item.type}                      |`)
+    });
+
+    readmeText = readme.join('\n');
+    fs.writeFileSync(`../COLAB.md`, readmeText);
+    return true;
+}
+
+(function () {
+    try {
+        copy_files();
+        make_readme();
+    } catch (e) {
+        console.error(e);
+    }
+})();
