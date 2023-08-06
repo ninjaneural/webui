875ef45163a976f59d6656ce07b4ef9c42cd3c2b
ninjaneural <127111336+ninjaneural@users.noreply.github.com>
2023-08-06 17:03:16 +0900

    sdxl dev update

--- misc/install_sdxl.sh
+++ misc/install_sdxl.sh
@@ -1,5 +1,5 @@
 pip install -q xformers==0.0.20 triton==2.0.0 -U
-git clone -b v1.5.1 https://github.com/AUTOMATIC1111/stable-diffusion-webui /content/$1
+git clone -b dev https://github.com/AUTOMATIC1111/stable-diffusion-webui /content/$1
 cd /content/$1
 sed -i -e "/from modules import launch_utils/a\import os" /content/$1/launch.py
 sed -i -e "/        prepare_environment()/a\        os.system(f\\\"\"\"sed -i -e \"s/dict()))/dict())).cuda()/g\" /content/$1/repositories/stable-diffusion-stability-ai/ldm/util.py\"\"\")" /content/$1/launch.py
