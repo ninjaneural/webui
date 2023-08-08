32f39ed8a7025eb024e2a0140d4c0a35f883e7cd
ninjaneural <127111336+ninjaneural@users.noreply.github.com>
2023-08-09 00:06:12 +0900

    google drive link

---
+++ misc/link_google_drive.sh
@@ -0,0 +1,31 @@
+if [$2]; then
+  !mkdir /content/drive/MyDrive/$3
+  !mkdir /content/drive/MyDrive/$3/output
+  !mkdir /content/drive/MyDrive/$3/checkpoint
+  !mkdir /content/drive/MyDrive/$3/lora
+  !mkdir /content/drive/MyDrive/$3/lycoris
+  !mkdir /content/drive/MyDrive/$3/embedding
+  !mkdir /content/drive/MyDrive/$3/hypernetwork
+  !mkdir /content/drive/MyDrive/$3/wildcards
+  !mkdir /content/$1/models/Lora
+  !mkdir /content/$1/models/LyCORIS
+  !mkdir /content/$1/models/hypernetworks
+  !rm /content/$1/embeddings/google
+  !rm /content/$1/models/Stable-diffusion/google
+  !rm /content/$1/models/Lora/google
+  !rm /content/$1/models/LyCORIS/google
+  !rm /content/$1/models/hypernetworks/google
+  !rm /content/$1/outputs
+  !mv /content/$1/outputs /content/$1/outputs_backup
+  !rm /content/$1/extensions/sd-dynamic-prompts/wildcards
+  !mv /content/$1/extensions/sd-dynamic-prompts/wildcards /content/$1/extensions/sd-dynamic-prompts/wildcards_backup
+  !ln -s /content/drive/MyDrive/$3/embedding /content/$1/embeddings/google
+  !ln -s /content/drive/MyDrive/$3/checkpoint /content/$1/models/Stable-diffusion/google
+  !ln -s /content/drive/MyDrive/$3/lora /content/$1/models/Lora/google
+  !ln -s /content/drive/MyDrive/$3/lycoris /content/$1/models/LyCORIS/google
+  !ln -s /content/drive/MyDrive/$3/hypernetwork /content/$1/models/hypernetworks/google
+  !ln -s /content/drive/MyDrive/$3/output /content/$1/outputs
+  !ln -s /content/drive/MyDrive/$3/wildcards /content/$1/extensions/sd-dynamic-prompts/wildcards
+else
+  !rm /content/$1/outputs
+fi
