d7d474c46c76074ba120cf4d7a33b30eec01e751
ninjaneural <127111336+ninjaneural@users.noreply.github.com>
2023-08-09 03:38:31 +0900

    deforum install

--- misc/install_deforum.sh
+++ misc/install_deforum.sh
@@ -1,22 +1,12 @@
 pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchtext==0.14.1 torchaudio==0.13.1 torchdata==0.5.1 --extra-index-url https://download.pytorch.org/whl/cu117
-if [ $3 == True ]; then
-  pip install -U openmim
-  mim install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu117/torch1.13/index.html
-  pip install mmdet==2.28.2
+
+git clone -b stable2 https://github.com/neuralninja22/sd-webui /content/$1
+if [ $2 == True ]; then
+  git clone -b stable1 https://github.com/ninjaneural/controlnet /content/$1/extensions/controlnet
 fi
 
-git clone -b stable1-controlnet https://github.com/neuralninja22/sd-webui /content/$1
-if [ $3 == True ]; then
-  git clone https://github.com/ninjaneural/ddetailer /content/$1/extensions/ddetailer
-fi
-if [ $4 == True ]; then
-  git clone https://github.com/Bing-su/adetailer /content/$1/extensions/adetailer
-fi
-
-git clone https://github.com/adieyal/sd-dynamic-prompts /content/$1/extensions/sd-dynamic-prompts
-git clone https://github.com/mcmonkeyprojects/sd-dynamic-thresholding /content/$1/extensions/sd-dynamic-thresholding
-git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete /content/$1/extensions/a1111-sd-webui-tagcomplete
-git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111 /content/$1/extensions/ultimate-upscale-for-automatic1111
+git clone https://github.com/KohakuBlueleaf/a1111-sd-webui-lycoris /content/$1/extensions/a1111-sd-webui-lycoris
+git clone -b nightly https://github.com/ninjaneural/deforum /content/$1/extensions/deforum
 
 if [ $2 == True ]; then
   aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_openpose_fp16.safetensors -d /content/$1/extensions/controlnet/models -o control_v11p_sd15_openpose_fp16.safetensors
