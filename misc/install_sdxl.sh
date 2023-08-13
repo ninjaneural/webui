87d049d769420998d819fe0cd86e50c254844f3e
ninjaneural <127111336+ninjaneural@users.noreply.github.com>
2023-08-13 17:33:25 +0900

    config update

--- misc/install_sdxl.sh
+++ misc/install_sdxl.sh
@@ -16,4 +16,4 @@ git clone https://github.com/wcde/sd-webui-refiner /content/$1/extensions/sd-web
 cd /content/$1
 sed -i -e "/from modules import launch_utils/a\import os" /content/$1/launch.py
 sed -i -e "/        prepare_environment()/a\        os.system(f\\\"\"\"sed -i -e \"s/dict()))/dict())).cuda()/g\" /content/$1/repositories/stable-diffusion-stability-ai/ldm/util.py\"\"\")" /content/$1/launch.py
-wget https://raw.githubusercontent.com/ninjaneural/webui/master/misc/config.json -O /content/$1/config.json
+wget https://raw.githubusercontent.com/ninjaneural/webui/master/misc/config_sdxl.json -O /content/$1/config.json
