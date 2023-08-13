c2b3db5ff74ea8e935df7f8de274d741e649a944
ninjaneural <127111336+ninjaneural@users.noreply.github.com>
2023-08-13 17:50:29 +0900

    example update

--- misc/example/install.sh
+++ misc/example/install.sh
@@ -2,4 +2,4 @@
 git clone https://github.com/KohakuBlueleaf/a1111-sd-webui-haku-img ./extensions/a1111-sd-webui-haku-img
 
 # 한글패치
-git clone https://github.com/36DB/stable-diffusion-webui-localization-ko_KR ./extensions/localization-ko_KR
+git clone https://github.com/36DB/stable-diffusion-webui-localization-ko_KR ./extensions/stable-diffusion-webui-localization-ko_KR
