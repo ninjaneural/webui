6c17c9e58a71397e60bdcf41eb86a10ee39c9cf2
NeuralNinja <ninjaneural@gmail.com>
2023-08-25 13:57:04 +0900

    animatediff readme

--- misc/link_google_drive_animatediff.sh
+++ misc/link_google_drive_animatediff.sh
@@ -2,12 +2,14 @@ if [ $2 == True ]; then
   mkdir /content/drive/MyDrive/$3
   mkdir /content/drive/MyDrive/$3/output
   mkdir /content/drive/MyDrive/$3/checkpoint
+  mkdir /content/drive/MyDrive/$3/lora
   mkdir /content/drive/MyDrive/$3/wildcards
-  rm /content/$1/embeddings
+  mkdir /content/$1/data/models/lora
+  rm /content/$1/data/embeddings
   rm /content/$1/data/models/sd
   rm /content/$1/output
   mv /content/$1/output /content/$1/output_backup
-  ln -s /content/drive/MyDrive/$3/embedding /content/$1/embeddings
+  ln -s /content/drive/MyDrive/$3/embedding /content/$1/data/embeddings/google
   ln -s /content/drive/MyDrive/$3/checkpoint /content/$1/data/models/sd/google
   ln -s /content/drive/MyDrive/$3/lora /content/$1/data/models/lora/google
   ln -s /content/drive/MyDrive/$3/output /content/$1/output
