d9fc8a69474f75eeae563937d15d8690a78642ca
NeuralNinja <ninjaneural@gmail.com>
2023-09-07 14:13:50 +0900

    install.sh update

--- misc/example/install.sh
+++ misc/example/install.sh
@@ -1,5 +1,5 @@
 # haku image 익스텐션
-git clone https://github.com/KohakuBlueleaf/a1111-sd-webui-haku-img ./extensions/a1111-sd-webui-haku-img
+# git clone https://github.com/KohakuBlueleaf/a1111-sd-webui-haku-img ./extensions/a1111-sd-webui-haku-img
 
 # 한글패치
-git clone https://github.com/36DB/stable-diffusion-webui-localization-ko_KR ./extensions/stable-diffusion-webui-localization-ko_KR
+# git clone https://github.com/36DB/stable-diffusion-webui-localization-ko_KR ./extensions/stable-diffusion-webui-localization-ko_KR
