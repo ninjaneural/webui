77cace0f8c825f95e273d72826ab0c65ffb2e574
NeuralNinja <ninjaneural@gmail.com>
2023-09-17 03:11:56 +0900

    update

--- misc/install_comfyui_sdxl.sh
+++ misc/install_comfyui_sdxl.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/comfyanonymous/ComfyUI /content/$1
+cd /content/$1
 pip install xformers!=0.0.18 -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu118 --extra-index-url https://download.pytorch.org/whl/cu117
 
 if [ $2 == True ]; then
