0ade16f468cc814a089904fb3092caf812b661f1
NeuralNinja <ninjaneural@gmail.com>
2023-09-17 03:06:15 +0900

    update

--- misc/install_comfyui.sh
+++ misc/install_comfyui.sh
@@ -1,6 +1,5 @@
-pip install xformers!=0.0.18 -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu118 --extra-index-url https://download.pytorch.org/whl/cu117
-
 git clone https://github.com/comfyanonymous/ComfyUI /content/$1
+pip install xformers!=0.0.18 -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu118 --extra-index-url https://download.pytorch.org/whl/cu117
 
 if [ $2 == True ]; then
   cd /content/$1/custom_nodes
