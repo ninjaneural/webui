9fd747be8e2fc209c313fea9b023ad363f85f3ec
NeuralNinja <ninjaneural@gmail.com>
2023-10-11 20:43:51 +0900

    comfyui install update

---
+++ memo/install_comfyui.md
@@ -0,0 +1,32 @@
+
+# 실행용 노트북 설명서
+
+install버전으로 최초 한번 설치한후에 이후에 **run.ipynb** 으로 설치없이 실행
+
+## 실행방법
+
+### 1. 구글드라이브에서 실행 (수정가능)
+
+> [run.ipynb 다운로드](https://github.com/ninjaneural/webui/blob/master/install/run.ipynb) 기본버전  
+
+1. run.ipyb 파일 다운로드 (클릭하고 오른쪽위에 다운로드버튼 클릭)
+
+2. 구글드라이브에 업로드
+
+3. 구글드라이브에 Colab앱 설치
+
+    <img src="./install/1.png" />
+
+    <img src="./install/2.png" />
+
+    <img src="./install/3.png" />
+
+
+3. 구글드라이브에서 실행 (run.ipynb 더블클릭)
+
+
+### 2. github에서 바로실행 (수정불가)
+
+> [run.ipynb 실행](https://colab.research.google.com/github/ninjaneural/webui/blob/master/install/run.ipynb) 기본버전  
+
+
--- script/template.js
+++ script/template.js
@@ -512,6 +512,20 @@ async function make_readme() {
     return true;
 }
 
+async function make_readme() {
+    let readme = [];
+    readme.push(`| 바로실행                                                                                                                                                                                        | 설치버전                                                                                                                                                                                        | Model                                                                                  | VAE  | Memo                    |`);
+    readme.push(`| ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | -------------------------------------------------------------------------------------- | ---- | ----------------------- |`);
+    const list = checkpoints.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 0);
+    list.forEach((item) => {
+        readme.push(`| [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/ninjaneural/webui/blob/master/comfyui/${item.ipynb}.ipynb) | [![Open In Colab](https://raw.githubusercontent.com/neuralninja22/colab/master/icons/colab-badge-install.svg)](https://colab.research.google.com/github/ninjaneural/webui/blob/master/install_comfyui/${item.ipynb}.ipynb) | [${item.name}](${item.model})                    | ${item.bakedVAE ? '' : '선택'} | ${item.type}                      |`)
+    });
+
+    readmeText = readme.join('\n');
+    fs.writeFileSync(`../COLAB_COMFYUI.md`, readmeText);
+    return true;
+}
+
 (function () {
     try {
         copy_files();
