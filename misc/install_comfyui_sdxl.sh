5f448d48a994b8560be40b0f7540e12a555821a9
NeuralNinja <ninjaneural@gmail.com>
2023-10-13 21:28:08 +0900

    update

--- misc/install_comfyui_sdxl.sh
+++ misc/install_comfyui_sdxl.sh
@@ -1,10 +1,3 @@
 git clone https://github.com/comfyanonymous/ComfyUI /content/$1
 cd /content/$1
 pip install xformers!=0.0.18 -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu118 --extra-index-url https://download.pytorch.org/whl/cu117
-
-if [ $2 == True ]; then
-  cd /content/$1/custom_nodes
-  git clone https://github.com/ltdrdata/ComfyUI-Impact-Pack
-fi
-
-cd /content/$1
