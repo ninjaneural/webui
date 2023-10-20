ee3a8fef1b97f18c55a80791725400783bc55f50
NeuralNinja <ninjaneural@gmail.com>
2023-10-20 15:18:45 +0900

    colab torch 2.1

--- misc/install_nightly.sh
+++ misc/install_nightly.sh
@@ -1,4 +1,5 @@
-pip install -q xformers==0.0.20 triton==2.0.0 -U
+pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118 -U
+pip install -q xformers==0.0.20
 
 if [ $4 == True ]; then
   pip install -U openmim
