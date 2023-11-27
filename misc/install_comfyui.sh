ab55d5ed5cf057de0b39e3d7c5fb52a1a545a1cd
NeuralNinja <ninjaneural@gmail.com>
2023-11-27 17:02:13 +0900

    update

--- misc/install_comfyui.sh
+++ misc/install_comfyui.sh
@@ -1,4 +1,3 @@
 git clone https://github.com/comfyanonymous/ComfyUI /content/$1
 cd /content/$1
-git reset --hard f12ec55983fb13b3bcad33b05ff8043b22b36181
 pip install xformers!=0.0.18 -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu118 --extra-index-url https://download.pytorch.org/whl/cu117
